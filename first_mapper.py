73e07421b9de3de25d9e1cca76c36aadb861c6e3
chatterburger97 <paromac@gmail.com>
2017-11-23 22:26:51 +0800

    large-image : upload the correct tile mapper v1

--- first_mapper.py
+++ first_mapper.py
@@ -1,27 +1,37 @@
+from __future__ import print_function
+import sys
+from StringIO import StringIO
+
 from pyspark import SparkContext
-from PIL import Image
 import numpy as np
+import cv2
+from PIL import Image
+
+img_as_array = lambda rawdata: np.asarray(Image.open(StringIO(rawdata)))
+get_type = lambda rdd_or_var: type(rdd_or_var)
+rgb_nparray_to_cvimg = lambda nparr: cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+def img_shape(cvimg):
+    height, width, something = np.asarray(cvimg.toLocalIterator()).shape
+    return height, width, something
 
 
-def mapper:
-    print "this is the mapper"
+def main():
+    sc = SparkContext(appName="tileMapper")
+    imgBinary = sc.binaryFiles("/user/bitnami/allsmiles.jpg")
+    img_rgb_matrix = imgBinary.mapValues(img_as_array)
+    img_rgb_matrix.collect()
 
+    print("XCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXXCXCXCXCXXCXCXCXCXCXCXCXCXCXCXCXXCXCXCXCXCXCXCXCXCXCXCX")
+    # img_rgb_matrix.foreach(print)
 
-def cropper(pilImage):
-    print "cropping the image to return an array of bytestrings"
+    cv_img = img_rgb_matrix.mapValues(rgb_nparray_to_cvimg) # cannot get shape
+    print("XCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXXCXCXCXCXXCXCXCXCXCXCXCXCXCXCXCXXCXCXCXCXCXCXCXCXCXCXCX",
+    img_shape(cv_img))
+    img_rgb_matrix.foreach(print)
+    # img_rgb_matrix.values().foreach(print)
 
+    sc.stop()
 
 if __name__ == '__main__':
-    sc = SparkContext(appName="largeImageTile")
-
-    imageBinary = sc.binaryFiles("/user/bitnami/calvin-being-funny.jpg")
-    #
-
-    image_to_array = lambda rawdata: np.asarray(Image.open(StringIO(rawdata)))
-    imageBinary.values().map(image_to_array)
-    imageBinary.cache()
-
-    # the imageBinary should now be a PIL image
-    # chop it up into cropped squares here
-
-    # convert the array of bytestrings into an RDD list
+    main()
