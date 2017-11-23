4760804853a3568f7aef7e1614bfd0528138f8da
chatterburger97 <paromac@gmail.com>
2017-11-24 01:28:13 +0800

    large-image : some more useless changes

--- first_mapper.py
+++ first_mapper.py
@@ -1,31 +1,34 @@
 from __future__ import print_function
-import sys
+
 from StringIO import StringIO
 
-from pyspark import SparkContext
-import numpy as np
-import cv2
 from PIL import Image
+
+import cv2
+
+import numpy as np
+
+from pyspark import SparkContext
 
 img_as_array = lambda rawdata: np.asarray(Image.open(StringIO(rawdata)))
 get_type = lambda rdd_or_var: type(rdd_or_var)
 rgb_nparray_to_cvimg = lambda nparr: cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+print_shape = lambda key, val: print(val.shape)
+
+
+def shape_of_nparray(nparr):
+    print(nparr)
+
 
 def main():
     sc = SparkContext(appName="tileMapper")
-    imgBinary = sc.binaryFiles("/user/bitnami/allsmiles.jpg")
-    img_rgb_matrix = imgBinary.mapValues(img_as_array)
-    img_rgb_matrix.collect()
+    img_binary = sc.binaryFiles("/user/bitnami/allsmiles.jpg")
 
-    print("XCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXXCXCXCXCXXCXCXCXCXCXCXCXCXCXCXCXXCXCXCXCXCXCXCXCXCXCXCX")
-    img_rgb_matrix.foreach(print)
+    img_rgb_matrix = img_binary.mapValues(img_as_array)
 
-    # cv_img = img_rgb_matrix.mapValues(rgb_nparray_to_cvimg) # cannot get shape
-    print("XCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXXCXCXCXCXXCXCXCXCXCXCXCXCXCXCXCXXCXCXCXCXCXCXCXCXCXCXCX",
-    # img_shape(cv_img))
-    # img_rgb_matrix.foreach(print)
-    # img_rgb_matrix.values().foreach(print)
-
+    print("XCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXXCXCXCXCX")
+    img_rgb_matrix.foreach(print_shape)
+    print("XCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXCXXCXCXCXC")
     sc.stop()
 
 if __name__ == '__main__':
