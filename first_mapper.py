a4ba473c0b49cad2555d64d3cfcad2fd04c3eed4
chatterburger97 <paromac@gmail.com>
2017-11-25 16:53:36 +0800

    large-image-cv2 : output correct values using explicit indexing

--- first_mapper.py
+++ first_mapper.py
@@ -2,12 +2,6 @@ from __future__ import print_function
 
 from fractions import gcd
 
-import scipy
-
-import sklearn.feature_extraction
-
-from sklearn.feature_extraction import image as skimage
-
 import math
 
 import cv2
@@ -20,14 +14,28 @@ import features
 
 from pyspark import SparkContext
 
+def compute_tile_avgs(cvimg):
+    block_width = gcd(cvimg.shape[0], cvimg.shape[1])
+    block_height = block_width
 
-def tile_mapper():
+    numrows = int(cvimg.shape[0]/block_height)
+    numcols = int(cvimg.shape[1]/block_width)
 
-    def tile_mapper_nested(tile_rgb_ndarray):
-        tile_as_img = cv2.imdecode(tile_rgb_ndarray, cv2.IMREAD_COLOR);
-        return [features.extractFeature(tile_as_img)]
+    tile_avgs = []
 
-    return tile_mapper_nested
+    for row in range(0, numrows):
+        for col in range(0, numcols):
+            y0 = row * block_height
+            y1 = y0 + block_height
+            x0 = col * block_width
+            x1 = x0 + block_width
+            tile_avg = features.extractFeature(cvimg[y0:y1, x0:x1])
+            tile_coords = [y0, y1, x0, x1]
+            tile_stats = (tile_coords, tile_avg)
+            tile_avgs.append(tile_stats)
+
+    return tile_avgs
+
 
 def extract_opencv_tiles():
 
@@ -36,36 +44,21 @@ def extract_opencv_tiles():
             imgfilename, imgbytes = imgfile_imgbytes
             nparr = np.fromstring(buffer(imgbytes), np.uint8)
             img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-            print(type(img), "is the type of the ndarray representaion of the image")
+            print(type(img), "is the type of the ndarray representation of the image")
             # height,width,channels = img.shape
             
             height, width, channels = img.shape
             print (height, width, channels , " is the shape of the image ndarray ")
             square_size = gcd(height, width)
-            
-            num_patches = int(math.floor(height * width / (4 * 4)))
-            
-            tiles = skimage.extract_patches_2d(img, (4, 4), max_patches=num_patches, random_state=0)
-            # tiles = []
-            # return tiles
-            print (type(tiles), "is the type of the object returned")
 
-            avg_list = []
-            for tile in tiles:
-                print(tile, " is a tile")
-                print(tile.shape, " is the shape of the tile")
-                print(type(tile), " is the type of the tie")
-                tile_img = cv2.imdecode(tile, cv2.IMREAD_COLOR)
-                print (tile_img, "is what cv read from the tile")
-                # print(tile_img)
-                curr_avg = features.extractFeature(tile_img)
-                # print(curr_avg, " is a feature")
-                avg_list.extend([curr_avg])
-            return features
+            img = cv2.resize(img, (width / square_size * square_size, height / square_size * square_size))
+            print (img.shape , " is the shape of the resized image ndarray ")
+
+            return compute_tile_avgs(img)
 
         except Exception, e:
             logging.exception(e)
-            return
+            return []
 
     return extract_opencv_tiles_nested
 
